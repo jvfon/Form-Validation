fe839eecb7c907a89c1d901b20a6f13c32dfdca3
Jose Fon <fonjosev@gmail.com>
2019-11-26 22:25:29 -0800

    error checking

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const name = document.getElementById('name')
 const name = document.getElementById('password')
 const name = document.getElementById('form')
 
-// in order to catch errors we need to add an event listener to the form
-form.addEventListener('submit', (e) => {  // adding the 'submit' event listener
-   e.preventDefault() // to prevent the page from submitting
+// 1- in order to catch errors we need to add an event listener to the form
+form.addEventListener('submit', (e) => {  // 1- adding the 'submit' event listener
+   let messages = []  // 2- checking for errors
+   if(name.value === '' || name.value == null) // 2- if the value of the name.value equals an empty string
+                          // or if the name.value equal to null 
+   e.preventDefault() // 1- to prevent the page from submitting if we have errors
 })
