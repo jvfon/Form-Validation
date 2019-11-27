01680acb486d508b277ef4e09ae6fd4125a40537
Jose Fon <fonjosev@gmail.com>
2019-11-26 22:31:33 -0800

    added more comments

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const name = document.getElementById('form')
 // 1- in order to catch errors we need to add an event listener to the form
 form.addEventListener('submit', (e) => {  // 1- adding the 'submit' event listener
    let messages = []  // 2- checking for errors
-   if(name.value === '' || name.value == null) // 2- if the value of the name.value equals an empty string
-                          // or if the name.value equal to null 
+    // 2- if the value of the name.value equals an empty string
+    // 2- or if the name.value equal to null 
+   // 2-  that means the user did not pass in any name
+   if(name.value === '' || name.value == null) 
+                        
    e.preventDefault() // 1- to prevent the page from submitting if we have errors
 })
