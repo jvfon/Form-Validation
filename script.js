4e314d50e69233d5b1f5cf46196c30385f90100e
Jose Fon <fonjosev@gmail.com>
2019-11-26 22:19:46 -0800

    prevent the page from submiting

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // first input name
 const name = document.getElementById('name')
 const name = document.getElementById('password')
+const name = document.getElementById('form')
+
+// in order to catch errors we need to add an event listener to the form
+form.addEventListener('submit', (e) => {  // adding the 'submit' event listener
+   e.preventDefault() // to prevent the page from submitting
+})
