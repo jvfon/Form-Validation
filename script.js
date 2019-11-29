bfaa24db8568e330d9765c80d8b491f8f20affc7
Jose Fon <fonjosev@gmail.com>
2019-11-28 17:05:58 -0800

    fixing variable names

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // first input name
 const name = document.getElementById('name')
-const name = document.getElementById('password')
-const name = document.getElementById('form')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+// 4- we create another variable
+const 
 
 // 1- in order to catch errors we need to add an event listener to the form
 form.addEventListener('submit', (e) => {  // 1- adding the 'submit' event listener
@@ -11,6 +13,14 @@ form.addEventListener('submit', (e) => {  // 1- adding the 'submit' event listen
    // 2-  that means the user did not pass in any name
    if (name.value === '' || name.value == null) {
       // 3- because of that we are going to send a message
+      messsage.push('Name is required')
+   }
+
+   // 4- if the length of the message is greater than 0 
+   if  (messages.legnth > 0 ) {
+      // 4- we want to prevent the actual form from submitting
+      // 4- and we want to send the errors to our error
+      e.preventDefault()
    }
                         
    e.preventDefault() // 1- to prevent the page from submitting if we have errors
