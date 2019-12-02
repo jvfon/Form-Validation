14cb289661ad60a4cc96ba9dd0ab08195eb48afe
Jose Fon <fonjosev@gmail.com>
2019-12-01 16:52:27 -0800

    variables

---
+++ script2.js
@@ -0,0 +1,5 @@
+// bring all the input fields
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const firstName = document.getElementById('firstName');
+const firstName = document.getElementById('firstName');
