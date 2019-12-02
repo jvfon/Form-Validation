ffa4502d2e6ebdb60cdf5457b06f1221847e16e4
Jose Fon <fonjosev@gmail.com>
2019-12-01 17:12:37 -0800

    set validate colors

--- script2.js
+++ script2.js
@@ -6,3 +6,13 @@ const confirmPassword = document.getElementById('confirmPassword');
 const email = document.getElementById('email');
 
 // form
+const form = document.getElementById('myForm');
+
+
+// set green and red  of the validation colors that materialize uses
+// to avoid putting them each time.
+const green = '#4CAF50';
+const red = '#F44336';
+
+// validate the first name
+
