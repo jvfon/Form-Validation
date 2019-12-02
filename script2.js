c1e922c673cc8361354c784829ad856225272b4e
Jose Fon <fonjosev@gmail.com>
2019-12-01 18:10:19 -0800

    validating the first name

--- script2.js
+++ script2.js
@@ -15,4 +15,19 @@ const green = '#4CAF50';
 const red = '#F44336';
 
 // validate the first name
+// arrow functions won't be used because they won't simply the syntax
+function validateFirstName() {
+   // check if the field is empty 
+   // using a function that hasn't been created yet.
+   // passing the whole 'firstName' field instead of only the value
+   // if the field is empty, then return. Don't check for anything else.
+   if (checkIfEmpty(firstName)) return;
+   // check if it has only letters
 
+}
+
+// creating the checkIfEmpty function
+// passing the 'field'
+function checkIfEmpty(field) {
+   if(checkIfEmpty(field))
+}
