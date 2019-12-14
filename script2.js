2fb7b75e54a90644480ff9d3b239c158c1e7752e
Jose Fon <fonjosev@gmail.com>
2019-12-14 13:57:44 -0800

    rewriting js

--- script2.js
+++ script2.js
@@ -1,6 +1,9 @@
 // bring all the input fields
+
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
+
+
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
 const email = document.getElementById('email');
