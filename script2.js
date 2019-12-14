1f41f594fe492f9071c24429c1837c902a713a04
Jose Fon <fonjosev@gmail.com>
2019-12-14 15:50:01 -0800

    line by line

--- script2.js
+++ script2.js
@@ -2,9 +2,11 @@
 
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
-
+const password = document.getElementById('password');
 
 const password = document.getElementById('password');
+
+
 const confirmPassword = document.getElementById('confirmPassword');
 const email = document.getElementById('email');
 
